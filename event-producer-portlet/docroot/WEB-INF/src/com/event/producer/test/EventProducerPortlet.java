afea29813e82d6b8e9342d186192b66e00ae3697
blade <4blade@rambler.ru>
2014-07-18 14:16:53 +1000

    Added JSON as required in the task.

--- event-producer-portlet/docroot/WEB-INF/src/com/event/producer/test/EventProducerPortlet.java
+++ event-producer-portlet/docroot/WEB-INF/src/com/event/producer/test/EventProducerPortlet.java
@@ -9,12 +9,26 @@ import javax.portlet.PortletException;
 import com.liferay.util.bridges.mvc.MVCPortlet;
 
 public class EventProducerPortlet extends MVCPortlet {
+    final String ACTION_A = "{\"ACTION\":{{\"ACTION_ID\":\"ActionA\"},\"ACTION_VALUE\":{\"NUM_OF_ACCOUNTS\":29814,\"TOTAL_PAID\":241863.6900000005,\"POSTAL\":3000,\"TOTAL_NOT_PAID\":29205.68999999981}}}";
+    final String ACTION_B = "{\"ACTION\":{{\"ACTION_ID\":\"ActionB\"},\"ACTION_VALUE\":{\"NUM_OF_ACCOUNTS\":5404,\"TOTAL_PAID\":33960.07000000003,\"POSTAL\":3001,\"TOTAL_NOT_PAID\":6758.070000000018}}}";
+    final String ACTION_C = "{\"ACTION\":{{\"ACTION_ID\":\"ActionC\"},\"ACTION_VALUE\":{\"NUM_OF_ACCOUNTS\":3903,\"TOTAL_PAID\":38716.60999999999,\"POSTAL\":3002,\"TOTAL_NOT_PAID\":3118.41}}}";
 
-    @Override
-    public void processAction(ActionRequest actionRequest, ActionResponse actionResponse) throws IOException,
+    public void fireActionA(ActionRequest actionRequest, ActionResponse actionResponse) throws IOException,
             PortletException {
-        actionResponse.setRenderParameter("id1", "EventA");
-        System.out.println("VYAFIMCHYK: Action processed!!!");
+        actionResponse.setRenderParameter("id1", ACTION_A);
+        System.out.println("VYAFIMCHYK: ActionA fired!!!");
+    }
+
+    public void fireActionB(ActionRequest actionRequest, ActionResponse actionResponse) throws IOException,
+            PortletException {
+        actionResponse.setRenderParameter("id1", ACTION_B);
+        System.out.println("VYAFIMCHYK: ActionB fired!!!");
+    }
+
+    public void fireActionC(ActionRequest actionRequest, ActionResponse actionResponse) throws IOException,
+            PortletException {
+        actionResponse.setRenderParameter("id1", ACTION_C);
+        System.out.println("VYAFIMCHYK: ActionC fired!!!");
     }
 
 }
--- event-producer-portlet/docroot/edit.jsp
+++ event-producer-portlet/docroot/edit.jsp
@@ -8,12 +8,28 @@
 This is the <b>Event Producer</b> portlet Edit Page.<br>
 Please, press a button below to fire the action:<br><br>
 
-<portlet:actionURL var="editFireActionURL">
+<portlet:actionURL var="fireActionA_URL" name="fireActionA">
+	<portlet:param name="mvcPath" value="/view.jsp" />
+</portlet:actionURL>
+
+<aui:form action="<%= fireActionA_URL %>" method="post">
+	<aui:button type="submit" value="Fire ActionA"></aui:button>
+</aui:form>
+
+<portlet:actionURL var="fireActionB_URL" name="fireActionB">
+	<portlet:param name="mvcPath" value="/view.jsp" />
+</portlet:actionURL>
+
+<aui:form action="<%= fireActionB_URL %>" method="post">
+	<aui:button type="submit" value="Fire ActionB"></aui:button>
+</aui:form>
+
+<portlet:actionURL var="fireActionC_URL" name="fireActionC">
 	<portlet:param name="mvcPath" value="/view.jsp"/>
 </portlet:actionURL>
 
-<aui:form action="<%= editFireActionURL %>" method="post">
-	<aui:button type="submit" value="Fire EventA"></aui:button>
+<aui:form action="<%= fireActionC_URL %>" method="post">
+	<aui:button type="submit" value="Fire ActionC"></aui:button>
 </aui:form>
 
 <portlet:renderURL var="viewFireActionURL">
